a7a4568d08dd9d85344451500c43229dbf23f580
KeenanOH <86394469+KeenanOH@users.noreply.github.com>
2024-02-20 20:33:05 -0600

    Fix order schema

--- src/models/orderDetail.ts
+++ src/models/orderDetail.ts
@@ -7,7 +7,8 @@ export const OrderDetail = z.object({
     id: z.string(),
     createdAt: z.date(),
     user: User,
-    items: z.array(Item)
+    items: z.array(Item),
+    completed: z.boolean()
 })
 
 export type OrderDetail = z.infer<typeof OrderDetail>
@@ -15,6 +16,7 @@ export type OrderDetail = z.infer<typeof OrderDetail>
 export const selectOrderDetail = {
     id: true,
     createdAt: true,
+    completed: true,
     user: {
         select: selectUser
     }
