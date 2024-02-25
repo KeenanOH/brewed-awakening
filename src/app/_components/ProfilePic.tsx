15844614c4f812f5d0c6daee9dd0b70e260b358d
Ewetzel5400 <90660042+Ewetzel5400@users.noreply.github.com>
2024-02-25 10:13:55 -0600

    Eric Wetzel Profile Picture and Body Text Commit

---
+++ src/app/_components/ProfilePic.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+interface ProfilePictureProps {
+    imageURL: string
+    alt: string
+    size?: number
+}
+
+const ProfilePicture: React.FC<ProfilePictureProps> = ({ imageURL, alt, size = 100 }) => {
+    const containerStyle: React.CSSProperties = {
+        width: size,
+        height: size,
+        display: "inline-block",
+        borderRadius: "50%",
+        overflow: "hidden"
+    }
+
+    const imageStyle: React.CSSProperties = {
+        width: "100%",
+        height: "auto"
+    }
+
+    return (
+        <div style={containerStyle}>
+            <img src={imageURL} alt={alt} style={imageStyle} />
+        </div>
+    )
+}
+
+export default ProfilePicture
