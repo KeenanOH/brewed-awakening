df3c234969fd5ef4e4ab627c52076334d2318af7
nishanpathak <npathak5348@stu.d214.org>
2024-03-02 16:53:15 -0600

    hi

---
+++ src/app/_components/SubheadingText.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading";
+
+export default function App() {
+    return (
+        <div>
+            <Heading>Subheading 1</Heading>
+            <Heading>Subheading 2</Heading>
+            <Heading>Subheading 3</Heading>
+        </div>
+    );
+}
---
+++ src/app/_components/ViewCart.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+type CheckoutButtonProps = {
+
+    onClick: () => void
+}
+
+export default function CheckoutButton({  onClick }: CheckoutButtonProps) {
+    return (
+        <div className={"text-[##319795]"} onClick={() => onClick}>
+            More -{">"}
+        </div>
+    )
+
+}
+
+
+
+
+
