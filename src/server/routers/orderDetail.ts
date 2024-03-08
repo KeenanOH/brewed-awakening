6504a7265bc46e9ae18d1be22a9a377e42f79791
pprajapati7964 <pprajapati4355@stu.d214.org>
2024-03-07 22:46:57 -0600

    getOrderDetail

--- src/server/routers/orderDetail.ts
+++ src/server/routers/orderDetail.ts
@@ -1,6 +1,24 @@
-import { router } from "@/server/trpc"
+import { z } from "zod"
+
+import { OrderDetail } from "@/models/orderDetail"
+import { publicProcedure, router } from "@/server/trpc"
 
 // note: you may need to search on prisma docs. Selecting the items needs an "includes" arg
 export const orderDetailRouter = router({
-
+    getOrderDetail: publicProcedure
+        .input(z.object({
+            id: z.string()
+        }))
+        .output(z.nullable(OrderDetail))
+        .query(  async ( { ctx , input  } ) => {
+            return ctx.prisma.order.findFirst({
+                where: {
+                    id: input.id
+                },
+                include: {
+                    user: true,
+                    items: true
+                }
+            })
+        })
 })
