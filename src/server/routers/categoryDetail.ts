3bfcdb5f9015b584ea6d625f18f352d22c3ad533
MeghP89 <mpatel5280@stu.d214.org>
2024-03-07 20:20:28 -0600

    added getcatergorydetail by id unauthenticated. There might be a problem with the model again, but there is a ctx error.

--- src/server/routers/categoryDetail.ts
+++ src/server/routers/categoryDetail.ts
@@ -1,6 +1,20 @@
-import { router } from "@/server/trpc"
+import {publicProcedure, router} from "@/server/trpc"
+import {z} from "zod"
+import {CategoryDetail, selectCategoryDetail} from "@/models/categoryDetail"
 
 // note: you may need to search on prisma docs. Selecting the items needs an "includes" arg
 export const categoryDetailRouter = router({
-
+    getCategoryDetail: publicProcedure
+        .input(z.object({
+            id: z.string()
+        }))
+        .output(z.nullable(CategoryDetail))
+        .query(async ({ ctx, input }) => {
+            return ctx.prisma.category.findFirst({
+                select: selectCategoryDetail,
+                where: {
+                    id: input.id
+                }
+            })
+        })
 })
