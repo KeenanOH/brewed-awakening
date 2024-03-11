4f102a976141584fe0eeca2a097ce6895a159f62
Keenan <86394469+KeenanOH@users.noreply.github.com>
2024-03-11 18:29:33 -0500

    Update userDetail.ts

--- src/server/routers/userDetail.ts
+++ src/server/routers/userDetail.ts
@@ -5,7 +5,7 @@ import { UserDetail, selectUserDetail } from "@/models/userDetail"
 
 // note: you may need to search on prisma docs. Selecting the items needs an "includes" arg
 export const userDetailRouter = router({
-    getUser: authenticatedProcedure
+    getUserDetail: authenticatedProcedure
         .input(z.object({
             id: z.string().optional(),
             email: z.string().optional(),
