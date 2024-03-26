c6b3028f6534e27547ddbb1ede97c385b9ca4a46
Subham1219 <spathak5540@stu.d214.org>
2024-03-26 18:02:21 -0500

    Checkouttext and orderdetailrow done

--- src/app/checkout/_components/CheckoutButton.tsx
+++ src/app/checkout/_components/CheckoutButton.tsx
@@ -1,33 +1,19 @@
-import { Button, Flex } from "@chakra-ui/react"
+import { Button, ButtonProps } from "@chakra-ui/react"
 import React from "react"
-import { FiArrowRight } from "react-icons/fi"
 
-type CheckoutButtonProps = {
+interface CheckoutButtonProps extends ButtonProps {
     label: string;
-    rightIcon?: boolean;
-    width: string;
-    height: string;
-    borderRadius: string;
-    padding: string;
+}
 
-};
-
-export default function CheckoutButton({ label, rightIcon, width, height,  borderRadius, padding }: CheckoutButtonProps){
+const CheckoutButton: React.FC<CheckoutButtonProps> = ({
+    label,
+    ...buttonProps // Spread the rest of the props
+}) => {
     return (
-        <Flex position="absolute" >
-            <Button
-                size="md"
-                variant="solid"
-                colorScheme="teal"
-                rightIcon={rightIcon ? <FiArrowRight /> : undefined}
-                width={width}
-                height={height}
-                borderRadius={borderRadius}
-                padding={padding}
-                cursor="pointer"
-            >
-                {label}
-            </Button>
-        </Flex>
+        <Button {...buttonProps}>
+            {label}
+        </Button>
     )
 }
+
+export default CheckoutButton
