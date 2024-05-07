4c2e2ef84fe41796891c83a35bb0f5b6d8d2cb8c
KeenanOH <86394469+KeenanOH@users.noreply.github.com>
2024-05-07 13:35:16 -0500

    Allow the ability to remove items from cart

--- src/app/_components/ItemRow.tsx
+++ src/app/_components/ItemRow.tsx
@@ -1,13 +1,30 @@
-import { Card, Text } from "@chakra-ui/react"
+import { Card, IconButton, Text } from "@chakra-ui/react"
+import { X } from "lucide-react"
 
+import { useCartStore } from "@/app/_stores/cartStore"
 import { Item } from "@/models/item"
 
-export default function ItemRow({ item }: { item: Item }) {
+export default function ItemRow({ item, removable = false }: { item: Item, removable?: boolean }) {
+
+    const cartStore = useCartStore()
+
     return (
         <Card className="p-4">
             <div className="flex items-center gap-x-4">
                 <img src={ item.imageUrl } alt={ item.name } className="h-10 w-10" />
                 <Text>{ item.name }</Text>
+
+                { removable &&
+                    <IconButton
+                        className="ml-auto"
+                        aria-label="Remove item"
+                        variant="ghost"
+                        icon={ <X /> }
+                        onClick={ () => {
+                            cartStore.remove(item.id)
+                        } }
+                    />
+                }
             </div>
         </Card>
     )
