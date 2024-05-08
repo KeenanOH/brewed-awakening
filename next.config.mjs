1a3165e1c128f192ece2ef44a50d947b7a78c51a
Keenan <86394469+KeenanOH@users.noreply.github.com>
2024-05-08 09:40:27 -0500

    PWA config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+});
+
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     }
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
